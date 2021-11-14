d50d4be962bba1be005314107c0cd5f1004f4a69
Vrindavani <74727948+DenizNazari@users.noreply.github.com>
2021-11-14 18:32:50 +0300

    odev

---
+++ ders1/basic-math.py
@@ -0,0 +1,103 @@
+# islemler :
+
+#toplama +
+a=10+3
+print(a)
+
+# #çıkarma -
+# a=10-3
+# print(a)
+
+# #çarpma *
+# a=10*3
+# print(a)
+
+# #bölme /
+# a=10/3
+# print(a)
+
+# #modül %
+# a=10%3
+# print(a)
+
+# #kalansız bölüm //
+# a=10//3
+# print(a)
+
+
+
+
+# #degisken adlandirma
+# a,s=20,30
+# print(a+s)
+
+# camelCase = "camelCase"
+
+
+
+
+
+
+
+
+
+# #veri tipi:
+
+# #int (integer)
+# a=10
+# print(a)
+# print(type(a))
+# #float (floating point)
+# b=10.5
+# print(b)
+# print(type(b))
+
+# #str (string)
+# c="deniz"
+# print(c)
+# print(type(c))
+
+# #bool (boolean)
+# d=True
+# print(d)
+# print(type(d))
+
+# #list (array)
+# e=[1,2,3,4,5]
+# print(e)
+# print(type(e))
+
+# #tuple (array)
+# f=(1,2,3,4,5)
+# print(f)
+# print(type(f))
+
+# #dict (dictionary)
+# g={1:"deniz",2:"mehmet",3:"ali"}
+# print(g)
+# print(type(g))
+
+# #set (set)
+# h={1,2,3,4,5}
+# print(h)
+
+
+
+
+
+
+
+# print(3+4)         # 7
+# print(3.0+4)       # 7.0
+# print(3.2+4)       # 7.2
+# print(type(3.0+4)) # <class 'float'>
+# print(3*3)         # 9
+# print(3.0*3)       #9.0
+# print(10/3)        # 3.3333333333333335
+# print(10/5)        # 2.0
+# print(type(10/5))  # <class 'float'>
+# print(3+5*2)       # 13
+# print((3+5)*2)     # 16
+# print(3+5*6/2)     # 18.0
+# print((3+5)*6/2)   # 24.0
+
