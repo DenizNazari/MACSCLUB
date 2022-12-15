6423cd68614b99c48d03689219191acd9a7ab470
Vrindavani <74727948+DenizNazari@users.noreply.github.com>
2022-12-15 13:56:51 +0300

    ders1

---
+++ ders1/odev.py
@@ -0,0 +1,53 @@
+# my_string adlı bir değişken oluşturun ve değerini "Merhaba, Dünya!" olarak ayarlayın.
+
+# my_string'in uzunluğunu almak için len() fonksiyonunu kullanarak sonucu string_length adlı bir değişkende saklayın.
+
+# my_string'in sonuna "Bu bir dizgidir." dizgisini birleştirmek için + operatörünü kullanarak sonucu my_concat adlı bir değişkende saklayın.
+
+# my_concat'i büyük harfe dönüştürmek için str.upper() metodunu kullanarak sonucu my_upper adlı bir değişkende saklayın.
+
+# my_upper'ı tek tek kelimelerden oluşan bir listeye ayırmak için str.split() metodunu kullanarak sonucu my_split adlı bir değişkende saklayın.
+
+# my_string içinde "WORLD" (büyük harf) kelimesini "python" (küçük harf) kelimesiyle değiştirmek için str.replace() metodunu kullanarak sonucu my_replace adlı bir değişkende saklayın.
+
+# İndekslemeyi kullanarak my_string'in 0. indeksindeki karakteri first_char adlı bir değişkende saklayın.
+
+# my_replace'deki başı ve sonundaki boşlukları kaldırmak için str.strip() metodunu kullanarak sonucu my_strip adlı bir değişkende saklayın.
+
+# my_strip içinde "Merhaba" dizgisinin bulunup bulunmadığını kontrol etmek için in anahtar kelimesini kullanarak sonucu hello_in_string adlı bir değişkende saklayın.
+
+# "Merhaba, {}!" dizgisindeki yer tutucuyu my_string değerine karşılık gelen değerle değiştirmek için str.format() metodunu kullanarak sonucu my_format adlı bir değişkende saklayın.
+
+
+
+
+
+
+
+
+
+
+
+'''
+    Daire Alanı   : πr²
+    Daire Çevresi : 2πr    
+    ** Yarı çapı verilen bir dairenin alan ve çevresini hesaplayınız. (π: 3.14)
+
+pi = 3.14
+
+r = float(input("yarı çap: "))
+
+alan = pi * (r ** 2)
+cevre = 2 * pi * r
+
+result = "alan: " + str(alan) + " çevre: " + str(cevre)
+print(result)
+
+'''
+
+'''
+    Bir aracın km cinsinden gittiği yol bilgisini mil olarak yazdırınız.
+    mil = km / 1.609344
+'''
+
+
---
+++ ders1/variable.py
@@ -0,0 +1,59 @@
+
+
+x = str(3)    # '3'
+y = int(3)    #  3
+z = float(3)  # 3.0
+
+
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+
+x = "John"
+# is the same as
+x = 'John'
+
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+
+
+x = "Python is awesome"
+print(x)
+
+
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+
